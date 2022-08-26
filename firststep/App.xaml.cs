29229586e399f8dc5815e94e34795ecf5832b5f8
suparuksa <suparuksa.dev@gmail.com>
2022-08-27 06:48:26 +0700

    begin first step for maui

---
+++ firststep/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace firststep;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ firststep/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace firststep;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
